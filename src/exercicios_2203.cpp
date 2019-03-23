8bce075006ef77bf218a5c0557813bf841e96c39
Igor Lemos Vicente <igorlemosvicente@gmail.com>
2019-03-22 21:42:34 -0300

    adiciona descrição de tarefas da dia 22 de março

---
+++ src/exercicios_2203.cpp
@@ -0,0 +1,15 @@
+/**
+ * 1 - Faça um programa que mostra um quadrado girando na origem. Ao redor dele, outro quadrado gira a uma certa distância da origem, porém rotacionando no próprio eixo em sentido contrário.
+ * Dica: rotating-cube e rotation
+ */
+
+
+/**
+ * 2 - Faça o demo "rotating-cube" rotacionar ao redor dos eixos x e y de forma automática
+ */
+
+
+/**
+ * 3 - Faça um retângulo (glutWireCube()) aumentar e diminuir de tamanho a cada 3 segundos.
+ * Dica: demo glut-update-delta
+ */
