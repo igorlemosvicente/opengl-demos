125a90fb91a5e6ae5ba58152c25df274d6c117ba
Igor Lemos Vicente <igorlemosvicente@gmail.com>
2019-05-13 20:36:41 -0300

    mais uma tentativa de textura

--- src/20190510_cubo_textura/main.cpp
+++ src/20190510_cubo_textura/main.cpp
@@ -42,7 +42,7 @@ void buildTextures();
 
 
 void init() {
-  glClearColor(0, 0, 0, 1);
+  glClearColor(1, 1, 1, 1);
 
   glShadeModel(GL_SMOOTH);
   glDepthFunc(GL_LESS);
@@ -50,7 +50,7 @@ void init() {
   glEnable(GL_DEPTH_TEST);
   glDisable(GL_CULL_FACE);
 
-  box_bmp_loader = new Bmp("/home/aluno-uffs/opengl-demos-igor/src/20190506_textura/box.bmp");
+  box_bmp_loader = new Bmp("/home/aluno-uffs/opengl-demos-igor/src/20190510_cubo_textura/crate.bmp");
   box_bmp_loader->convertBGRtoRGB();
   box_image = box_bmp_loader->getImage();
 
@@ -65,21 +65,40 @@ void init() {
 
 void buildTextures() {
   //GL_REPEAT e mipmap
-  glGenTextures(1, texturas);
+//  glGenTextures(1, texturas);
+//  glBindTexture(GL_TEXTURE_2D, texturas[0]);
+//
+//  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+//  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+//
+//  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_NEAREST);
+//  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+//  gluBuild2DMipmaps(GL_TEXTURE_2D,
+//                    GL_RGB,
+//                    box_bmp_loader->getWidth(),
+//                    box_bmp_loader->getHeight(),
+//                    GL_RGB,
+//                    GL_UNSIGNED_BYTE,
+//                    box_image);
+
+  glGenTextures(1, &texturas[0]);
   glBindTexture(GL_TEXTURE_2D, texturas[0]);
 
-  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
-  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP);
+  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP);
 
-  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_NEAREST);
-  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
-  gluBuild2DMipmaps(GL_TEXTURE_2D,
-                    GL_RGB,
-                    box_bmp_loader->getWidth(),
-                    box_bmp_loader->getHeight(),
-                    GL_RGB,
-                    GL_UNSIGNED_BYTE,
-                    box_image);
+  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+
+  glTexImage2D(GL_TEXTURE_2D,
+               0,
+               GL_RGB,
+               box_bmp_loader->getWidth(),
+               box_bmp_loader->getHeight(),
+               0,
+               GL_RGB,
+               GL_UNSIGNED_BYTE,
+               box_image);
 }
 
 ////////////////////////////////////////////////////////////////////////////////////////
@@ -87,6 +106,7 @@ void display(void) {
   float abertura = 35;
   float znear = 1;
   float zfar = 200;
+  GLdouble square_size = 2;
 
   glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
@@ -97,28 +117,28 @@ void display(void) {
   glMatrixMode(GL_MODELVIEW);
 
   glLoadIdentity();
-  glTranslated(0, 0, -10);
+  glTranslated(square_size / -2.0, square_size / -2.0, -10);
   glPushMatrix();
 
-  glBindTexture(GL_TEXTURE_2D, texturas[0]);
 
   glDisable(GL_TEXTURE_GEN_S);
   glDisable(GL_TEXTURE_GEN_T);
   glBegin(GL_QUADS);
 
   GLfloat tex_coord = 1.5;
+  GLdouble texel_size = 1;
 
-  glTexCoord2f(0.5, 0);
-  glVertex3f(0.0f, 0.0f, 0);
+  glTexCoord3d(0, 0, 0);
+  glVertex3d(0.0, 0.0, 0);
 
-  glTexCoord2f(1.5, 0);
-  glVertex3f(1.0f, 0.0f, 0);
+  glTexCoord3d(texel_size, 0, 0);
+  glVertex3d(square_size, 0.0, 0);
 
-  glTexCoord2f(1.5, 1);
-  glVertex3f(1.0f, 1.0f, 0);
+  glTexCoord3d(texel_size, texel_size, 0);
+  glVertex3d(square_size, square_size, 0);
 
-  glTexCoord2f(0.5, 1);
-  glVertex3f(0.0f, 1.0f, 0);
+  glTexCoord3d(0, texel_size, 0);
+  glVertex3d(0, square_size, 0);
   glEnd();
   glPopMatrix();
 
@@ -133,7 +153,7 @@ int main(int argc, char *argv[]) {
   glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB | GLUT_DEPTH);
 
   glutInitWindowSize(SCREEN_X, SCREEN_Y);
-  glutInitWindowPosition(500, 500);
+  glutInitWindowPosition(500, 200);
   glutCreateWindow("Texture Demo");
   init();
 
