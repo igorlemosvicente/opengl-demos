8c72dcee31e8e20be5f00a49c8fbbace91703609
Igor Lemos Vicente <igorlemosvicente@gmail.com>
2019-06-07 21:35:31 -0300

    Do 0: Adiciona primeira versão da main

---
+++ src/opengl-moderno/main.cpp
@@ -0,0 +1,14 @@
+/**
+ * Basic input features of GLFW, including handling of mouse and keyboard input.
+ */
+
+#include "glad.h"
+#include <iostream>
+#include <GLFW/glfw3.h>
+
+#define WIDTH 500
+#define HEIGHT 500
+
+int main() {
+  return 0;
+}
